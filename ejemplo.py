48577a1fa509c91a27f660beea490198a1980313
Javier Vega Correa <javier@debian>
2021-05-17 11:14:52 +0200

    .

--- ejemplo.py
+++ ejemplo.py
@@ -8,14 +8,13 @@ URL_BASE="https://livescore-api.com/api-client/"
 KEY=os.environ["key"]
 SECRET=os.environ["secret"]
 payload={'key':KEY,'secret':SECRET}
-r=requests.get(URL_BASE+"matches/stats.json?match_id=172252&",params=payload)
+r=requests.get(URL_BASE+"countries/list.json",params=payload)
 doc=r.json()
 
 print(type(doc))
 
-print(doc)
+for info in doc["data"]["country"]:
+    print(info["federation"]["name"])
 
+#AFC,CAF,UEFA,CONMEBOL,FIFA
 
-#(URL_BASE"matches/stats.json?match_id=172252&key="+KEY+"&secret="+SECRET)
-
-#match_id=172252&
