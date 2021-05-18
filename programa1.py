48f95cf2268c26c3ecbbf04b2206893bbb445e92
Javier Vega Correa <javier@debian>
2021-05-18 15:27:33 +0200

    Programa 1 terminado

--- programa1.py
+++ programa1.py
@@ -1,11 +1,12 @@
 #Enunciado:
-#Este programa será un administrador de información sobre la champions league, en el cúal podrás consultar los partidos. 
+#Este programa muestra los grupos de la champions league. 
 
 #Librerias necesarias
 import requests
 import json
 import os
 from tabulate import tabulate
+
 #Declaración de variables
 URL_BASE="https://livescore-api.com/api-client/"
 KEY=os.environ["key"]
@@ -35,25 +36,31 @@ for info in dic_info_competiciones["data"]["competition"]:
         payload["competition_id"]=info["id"]
 
 
-#for grupos in lista_grupos:
-payload["group"]="A"
-r_champions_league=requests.get(URL_BASE+'leagues/table.json',params=payload)
-dic_champions_league=r_champions_league.json()
-datos=[]
-for info in dic_champions_league["data"]["table"]:
-    lista=[]
-    lista=[info["rank"],info["name"],info["points"],info["goal_diff"]]
-    datos.append(lista)
+#PROGRAMA
+print()
+print("Bienvenido al programa")
+print()
+a=input("Si quieres ver los grupos de la champions pulsa enter ")
+if a=="":
+    ##MOSTRAR GRUPOS
+    print("                             UEFA CHAMPIONS LEAGUE")
+    print()
+    for grupos in lista_grupos:
+        payload["group"]=grupos
+        r_champions_league=requests.get(URL_BASE+'leagues/table.json',params=payload)
+        dic_champions_league=r_champions_league.json()
+        datos=[]
+        for info in dic_champions_league["data"]["table"]:
+            lista=[]
+            lista=[info["rank"],info["name"],info["points"],info["goal_diff"]]
+            datos.append(lista)
+        print()
+        print("                          GRUPO %s" % grupos)
+        print(tabulate(datos,headers=['Puesto','Equipo','Puntos','Goal Average'],tablefmt='fancy_grid',stralign='center',floatfmt='.89f'))
+        payload.pop('group',grupos)
 
+print()
+print("Fin del programa")
+print()
 
-print("                          GRUPO %s" % lista_grupos[4])
-print(tabulate(datos,headers=['Puesto','Equipo','Puntos','Goal Average'],tablefmt='fancy_grid',stralign='center',floatfmt='.89f'))
-
-    
-
-
-
-#https://livescore-api.com/api-client/leagues/table.json?key=BvMEuAZzpZCSFZaJ&secret=xmgkAwKEopD0wXa8tWMYxMxWMaPr9ZbH&competition_id=244&group=A
-
-#https://livescore-api.com/api-client/competitions/list.json?key=BvMEuAZzpZCSFZaJ&secret=xmgkAwKEopD0wXa8tWMYxMxWMaPr9ZbH
 
--- prueba.py
+++ prueba.py
@@ -1,3 +1,3 @@
-from tabulate import tabulate
-titulo=[["GRUPO A"],["betis","puntos","dife"]]
-print(tabulate(titulo,headers="firstrow",tablefmt='fancy_grid',stralign='center',floatfmt='.0f'))
+import os
+
+print(os.environ(figlet Champions League))
