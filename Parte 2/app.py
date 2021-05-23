a5041b563dc15a013e459b6cf55eedfd54e64501
Javier Vega Correa <javier@debian>
2021-05-23 18:30:47 +0200

    champions funciona

--- Parte 2/app.py
+++ Parte 2/app.py
@@ -11,43 +11,45 @@ KEY=os.environ["key"]
 SECRET=os.environ["secret"]
 payload={'key':KEY,'secret':SECRET}
 
-##Lista competiciones
-r_competiciones=requests.get(URL_BASE+"countries/list.json",params=payload)
-dic_competiciones=r_competiciones.json()
-for info in dic_competiciones["data"]["country"]:
-    if info["name"]=="Champions League":
-        cad=info["leagues"]
-        payload["country"]=cad[-2:]
-r_champions_league=requests.get(URL_BASE+"leagues/list.json",params=payload)
-dic_grupos_champions_league=r_champions_league.json()
-lista_grupos=[]
-for info in dic_grupos_champions_league["data"]["league"]:
-    if info["name"].startswith("Gr"):
-        lista_grupos.append(info["name"][-1:])
-        payload.pop('country',84)
-##Información competiciones
-r_info_competiciones=requests.get(URL_BASE+'competitions/list.json',params=payload)
-dic_info_competiciones=r_info_competiciones.json()
-for info in dic_info_competiciones["data"]["competition"]:
-    if info["name"]=="Champions League" and info["federations"][0]["name"]=="UEFA":
-        payload["competition_id"]=info["id"]
-for grupos in lista_grupos:
-    payload["group"]=grupos
-    r_champions_league=requests.get(URL_BASE+'leagues/table.json',params=payload)
-    dic_champions_league=r_champions_league.json()
-    datos=[]
-    for info in dic_champions_league["data"]["table"]:
-        lista=[]
-        lista=[info["rank"],info["name"],info["points"],info["goal_diff"]]
-        datos.append(lista)
+
 
 @app.route('/',methods=["GET"])
 def inicio():
 	return render_template("inicio.html")
 
+          
 @app.route('/champions',methods=["GET"])
-def champions_league():	
-	return render_template("champions.html",datos=datos)
+def champions_league():
+    ##Lista competiciones
+    r_competiciones=requests.get(URL_BASE+"countries/list.json",params=payload)
+    dic_competiciones=r_competiciones.json()
+    for info in dic_competiciones["data"]["country"]:
+        if info["name"]=="Champions League":
+            cad=info["leagues"]
+            payload["country"]=cad[-2:]
+    r_champions_league=requests.get(URL_BASE+"leagues/list.json",params=payload)
+    dic_grupos_champions_league=r_champions_league.json()
+    lista_grupos=[]
+    for info in dic_grupos_champions_league["data"]["league"]:
+        if info["name"].startswith("Gr"):
+            lista_grupos.append(info["name"][-1:])
+            payload.pop('country',84)
+    ##Información competiciones
+    r_info_competiciones=requests.get(URL_BASE+'competitions/list.json',params=payload)
+    dic_info_competiciones=r_info_competiciones.json()
+    for info in dic_info_competiciones["data"]["competition"]:
+        if info["name"]=="Champions League" and info["federations"][0]["name"]=="UEFA":
+            payload["competition_id"]=info["id"]
+    for grupos in lista_grupos:
+        payload["group"]=grupos
+        r_champions_league=requests.get(URL_BASE+'leagues/table.json',params=payload)
+        dic_champions_league=r_champions_league.json()
+        datos=[]
+        for info in dic_champions_league["data"]["table"]:
+            lista=[]
+            lista=[info["rank"],info["name"],info["points"],info["goal_diff"]]
+            datos.append(lista)
+    return render_template("champions.html",datos=datos)
 #port=os.environ["PORT"]
 #'0.0.0.0',int(port)
 app.run(debug=True)
