1b2903e31ae0a38358cbeb6587e30e5b8320e2f1
Javier Vega Correa <javier@debian>
2021-05-30 19:50:47 +0200

    organización

--- app_ejemplo.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-from flask import Flask, render_template,abort,request
-app = Flask(__name__)
-
-@app.route('/',methods=["GET"])
-def inicio():
-	return render_template("inicio.html")
